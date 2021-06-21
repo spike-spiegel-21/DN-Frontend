be69e940639fead9a4650e9d1749259a11b4d2a1
Mayank Solanki <bizmiku@gmail.com>
2021-06-21 21:23:41 +0530

    1, 2, 7, 5 ADDED

--- Day 6/script.js
+++ Day 6/script.js
@@ -1,72 +1,65 @@
 /* ====================Task-1========================*/
 
-let x = [1,2,3,4,5,6];
+let x = [1, 2, 3, 4, 5, 6];
 let y = "Mayank";
 
-function is_array(m){
-  return m instanceof Array;
+function is_array(m) {
+    return m instanceof Array;
 }
 
-console.log(is_array(x))
+console.log(is_array(x));
 //useful link: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/instanceof
-
-
 
 /* ====================Task-2========================*/
 
-function clone_array(m){
-  mcpy = [...m];
-  console.log(mcpy)
+function clone_array(m) {
+    mcpy = [...m];
+    console.log(mcpy);
 }
 
-clone_array(x)
+clone_array(x);
 
-//useful link: https://www.freecodecamp.org/news/how-to-clone-an-array-in-javascript-1d3183468f6a/ 
-
-
+//useful link: https://www.freecodecamp.org/news/how-to-clone-an-array-in-javascript-1d3183468f6a/
 
 /* ====================Task-3========================*/
 
-function first(x, from=0){
-
-  // if(from<0) return x;
-  x.splice(from+1, x.length)
-  console.log(x)
+function first(x, from = 0) {
+    // if(from<0) return x;
+    x.splice(from + 1, x.length);
+    console.log(x);
 }
 
-first([7, 9, 0, -2], 6)
+first([7, 9, 0, -2], 6);
 
 //use of splice function w/o extra space
 
 /* ====================Task-4========================*/
 
-let arr = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+let arr = [3, "a", "a", "a", 2, 3, "a", 3, "a", 2, 4, 9, 3];
 
-function printit(arr){
-  let s =""
-  for(let i=0; i<arr.length; ++i){
-    s = s + arr[i] + "+"
-  }
+function printit(arr) {
+    let s = "";
+    for (let i = 0; i < arr.length; ++i) {
+        s = s + arr[i] + "+";
+    }
 
-  s1=s.slice(0,s.length-1);
-  return s1;
+    s1 = s.slice(0, s.length - 1);
+    return s1;
 }
 
-
-console.log(printit(arr))
-
+console.log(printit(arr));
 
 /* ====================Task-5========================*/
 
 var map = {};
 var mostFrequentElement = arr[0];
-function findMostFrequent(){
-    for(var i = 0; i<arr.length; i++){
-        if(!map[arr[i]]){
-            map[arr[i]]=1;
-        }else{
+function findMostFrequent() {
+    for (var i = 0; i < arr.length; i++) {
+        if (!map[arr[i]]) {
+            map[arr[i]] = 1;
+        } else {
             ++map[arr[i]];
-            if(map[arr[i]]>map[mostFrequentElement]){
+            if (map[arr[i]] > map[mostFrequentElement]) {
                 mostFrequentElement = arr[i];
             }
         }
@@ -74,4 +67,4 @@ function findMostFrequent(){
     return [mostFrequentElement, map[mostFrequentElement]];
 }
 let res = findMostFrequent(arr);
-console.log(`${res[0]} (${res[1]} times)`)
+console.log(`${res[0]} (${res[1]} times)`);
